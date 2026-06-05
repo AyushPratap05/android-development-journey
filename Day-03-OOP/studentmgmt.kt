44e937e42e2ed83bc32b91de2706e0e244ad543a
AyushPratap05 <ayushrajawat2005@gmail.com>
2026-06-05 22:08:45 +0530

    day3 some part

--- Day-03-OOP/studentmgmt.kt
+++ Day-03-OOP/studentmgmt.kt
@@ -19,3 +19,52 @@ interface Manageable{
     fun findById(id:Int): Student?
     fun displayAll()
 }
+abstract class BaseSystem(val systemName: String){
+    protected val students = mutableListOf<Student>() // use of protected ?
+
+    abstract fun generateReport()
+
+    fun getTotalCount() = students.size
+
+    fun displayHeader() {
+        println("╔══════════════════════════════════════════════╗")
+        println("║     $systemName")
+        println("╚══════════════════════════════════════════════╝")
+     }
+
+}
+
+
+// Main Mgmt Class 
+
+class StudentManagementSystem:
+BaseSystem("  🎓 STUDENT MANAGEMENT SYSTEM      ")
+Manageable{
+    companion object{
+        private var lastId = 100 
+        fun generateId() = ++lastId
+    }
+
+// CRUD OPR
+
+override fun add(student : Student){
+    students.add(student)
+    println("✅ Added: ${student.name} (ID: ${student.id})")
+}
+
+override fun remove(id:Int):Boolean{
+    val student = findById(id)
+    return if(student!=null){
+        students.remove(student)
+        println("🗑 Removed: ${student.name}")
+        true
+    }else{println("❌ Student with ID $id not found")
+            false
+        }
+
+}
+override fun findById(id:Int):Student?{
+    return students.find{it.id == id}
+}
+}
+
